9b93a2ffe997a493fb7b54d6ab642e12035b93c0
Jonathan Chen <04jono@gmail.com>
2024-11-06 11:56:27 -0500

    llm read in function bank

--- model.py
+++ model.py
@@ -1,24 +1,21 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+pipe = pipeline("text-generation", "meta-llama/Llama-3.2-1B-Instruct", tokenizer=tokenizer, device="cuda")
 
 def generate_text(prompt):
     messages = [
         {"role": "system", "content": '''
                     You will write a function that transforms an image such that it will
                     be easier to detect spots in it. You will be prompted with example functions, write
-                    a different function that is similar. Only return the function itself. The function must have
+                    a different function that is not the same concept. Only return the function itself. The function must have
                     two arguments: image (numpy.array) and clip (boolean).
          '''},
         {"role": "user", "content": prompt},
-    ]
-    tokenized_chat = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+    ]    
+    outputs = pipe(messages, return_full_text=False, max_new_tokens=512) 
     
-    outputs = model.generate(tokenized_chat, max_new_tokens=512) 
-
-    return tokenizer.decode(outputs[0])
+    return outputs[0]["generated_text"]
 
 def read_file_to_string(file_path):
     with open(file_path, 'r') as file:
@@ -27,4 +24,5 @@ def read_file_to_string(file_path):
 
 
 function_bank = read_file_to_string('function_bank.py')
-print(generate_text(""))
+
+print(generate_text(function_bank))
